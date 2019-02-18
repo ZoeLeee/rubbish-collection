cfc424d01ae4b696e719fc2df28b8151b2603cec
Zoe <lizhou1021@qq.com>
2019-02-18 21:40:25 +0800

    更新配置

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ render(App)
 
 if (module.hot) {
     module.hot.accept('./App.jsx', () => { // 当我们热更新的代码出现的时候，把App重新加载
-        const NextApp = require('./App.jsx').default //因为在App里使用的是export default语法，这里使用的是require,默认不会加载default的，所以需要手动加上
+        const NextApp = require('./App.jsx').default; //因为在App里使用的是export default语法，这里使用的是require,默认不会加载default的，所以需要手动加上
         render(NextApp) // 重新渲染到 document 里面
     })
 }
