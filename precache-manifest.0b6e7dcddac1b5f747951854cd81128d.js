de6640c271da9b883cb1ce492acaee60371cdce5
Zoe <lizhou1021@qq.com>
2019-02-23 15:29:48 +0800

    Updates

---
+++ precache-manifest.0b6e7dcddac1b5f747951854cd81128d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "30c83a4e6b3224b0b0cc",
+    "url": "/static/js/main.30c83a4e.chunk.js"
+  },
+  {
+    "revision": "b31057366b3c0c1c6a39",
+    "url": "/static/js/2.b3105736.chunk.js"
+  },
+  {
+    "revision": "30c83a4e6b3224b0b0cc",
+    "url": "/static/css/main.0684b301.chunk.css"
+  },
+  {
+    "revision": "b31057366b3c0c1c6a39",
+    "url": "/static/css/2.4e9a31e7.chunk.css"
+  },
+  {
+    "revision": "00ba76b38d9cb626a9212b0ee7a1120d",
+    "url": "/index.html"
+  }
+];
